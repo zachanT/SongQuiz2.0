c7365bab49c0fecef8fd934292b948d5e06cf993
zachanT <zacharyTchan@gmail.com>
2022-01-31 17:41:22 -0800

    Trying to fix for deploy

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/client/public'))
    .use(cors())
    .use(cookieParser())
 
