fa9c0b822c5eee1fa2f245b349a4ed84a2f60bf0
zachanT <zacharyTchan@gmail.com>
2022-01-31 17:34:36 -0800

    Trying to fix for deploy

--- server.js
+++ server.js
@@ -184,12 +184,12 @@ app.get('/', (req, res) => {
   res.send(playlists)
 })
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'))
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   })
+// }
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`)
