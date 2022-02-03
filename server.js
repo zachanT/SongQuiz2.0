71ef99d0bc77a3c6abf8da8b03caf1d37fa606f1
zachanT <zacharyTchan@gmail.com>
2022-02-02 16:09:33 -0800

    Attempting to deploy

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ app.get('/', (req, res) => {
 })
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static(path.resolve(__dirname, './client/build')))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
